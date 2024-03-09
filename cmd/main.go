7833600f8fb418b2eb136941b00ab50040c8407f
xunterr <artem.oxotnik@gmail.com>
2024-03-09 21:05:14 +0200

    Flags support

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/codecrafters-io/redis-starter-go/internal/server"
 	"github.com/codecrafters-io/redis-starter-go/internal/storage"
 )
 
+var (
+	PORT = 6379
+)
+
+func init() {
+	flag.IntVar(&PORT, "port", PORT, "Port number")
+}
+
 func main() {
+	flag.Parse()
+
 	sv := server.NewServer()
 	storage := storage.NewStorage()
 	server.Route(sv, *storage)
-	sv.Listen(":6969")
+	sv.Listen(fmt.Sprintf(":%d", PORT))
 }
