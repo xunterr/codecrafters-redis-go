d1a2f8b3c3ad7e79403c7f52661ffed1a554c5ba
xunterr <artem.oxotnik@gmail.com>
2024-04-06 22:23:59 +0300

    Even more logs...

--- internal/server/replication.go
+++ internal/server/replication.go
@@ -170,7 +170,8 @@ func setCapabilities(c net.Conn) {
 func psync(c net.Conn) {
 	log.Println("Psync")
 	client.Send(c, []string{"PSYNC", "?", "-1"})
-	_, err := client.Read(c)
+	r, err := client.Read(c)
+	log.Printf("Received data: %s", r)
 	if err != nil {
 		return
 	}
