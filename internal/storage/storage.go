cf4823fe14c6401968a8e68fc12f87d2210e4440
xunterr <artem.oxotnik@gmail.com>
2024-05-09 14:32:38 +0300

    Concurrent storage access

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,10 +3,12 @@ package storage
 import (
 	"errors"
 	"log"
+	"sync"
 	"time"
 )
 
 type Storage struct {
+	mu      sync.RWMutex
 	storage map[string]string
 }
 
@@ -27,24 +29,35 @@ func (s *Storage) SetWithTimer(key string, value string, expire int) error {
 		timer := time.After(expiryMs)
 		log.Printf("Expiry (ms): %d", expire)
 		<-timer
+		s.mu.Lock()
 		delete(s.storage, key)
+		s.mu.Unlock()
 	}(expire, key)
 	return nil
 }
 
 func (s *Storage) Set(key string, value string) error {
 	log.Printf("SET: %s - %s", key, value)
-	if _, ok := s.storage[key]; ok {
-		log.Printf("%v", s.storage)
+
+	s.mu.RLock()
+	_, ok := s.storage[key]
+	s.mu.RUnlock()
+	if ok {
 		return errors.New("Key already exists")
 	}
+
+	s.mu.Lock()
 	s.storage[key] = value
+	s.mu.Unlock()
 	return nil
 }
 
 func (s *Storage) Get(key string) (string, error) {
 	log.Printf("GET: %s", key)
+
+	s.mu.RLock()
 	value, ok := s.storage[key]
+	s.mu.RUnlock()
 	if !ok {
 		log.Printf("%v", s.storage)
 		return "", errors.New("No such key")
